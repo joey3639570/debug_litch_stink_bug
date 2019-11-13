6e4e47043aee85b8369823f4867758fdfb8f471b
sky712345678 <37875281+sky712345678@users.noreply.github.com>
2019-11-13 16:17:39 +0800

    Fix the problem that giving wrong <filename>
    Some changes making it easier to use.

--- DataAugmentation.py
+++ DataAugmentation.py
@@ -4,28 +4,32 @@ import xml.etree.ElementTree as ET
 import os
 from imgaug import augmenters as iaa
 
-# settings
+# settings settings
+
 # directory of images to augment
 dataDirectoryAddress = 'E:\\test'
 # augmentat techniques. Visit https://github.com/aleju/imgaug to find more techniques.
 seq = iaa.Sequential(
-    [iaa.Noop()])  # modify "iaa.Add(-45), iaa.Crop(precent=0.2), iaa.GaussianBlur(2)" to get the augmentation type you want
-#iaa.Add(-45), iaa.Crop(px=(30, 40, 30, 40), keep_size=False), iaa.GaussianBlur(2)
+    [iaa.Rot90(k=1, keep_size=False)])  # modify "iaa.Add(-45), iaa.Crop(precent=0.2), iaa.GaussianBlur(2)" to get the augmentation type you want
 # file names to augmented images
-fileNameToAdd = '_Noop'
-# settings
+fileNameToAdd = '_Rot90'
+# file type
+# be aware of the file type, ex: jpg v.s. JPG and jpg v.s. jpeg, they are different!
+fileType = '.jpg'
+
+# settings settings
 
 # getting fileNames of images to augment
 imageAddresses = []
 for f in os.listdir(dataDirectoryAddress):
-    if (f.__contains__('.jpg')):
+    if (f.__contains__(fileType)):
         imageAddresses.append(dataDirectoryAddress + '\\' + f)
 
 # augmentation
 for address in imageAddresses:
     boundingBoxes = []
     image = imageio.imread(address)
-    tree = ET.parse(address.replace('.jpg', '.xml'))
+    tree = ET.parse(address.replace(fileType, '.xml'))
     root = tree.getroot()
     # getting bounding boxes
     for obj in root.iter('object'):
@@ -40,12 +44,16 @@ for address in imageAddresses:
     # augmentedBBS = augmentedBBS.clip_out_of_image()
 
     for fileName in root.iter('filename'):
-        newName = str(fileName.text.replace('.jpg', fileNameToAdd)) + ".jpg"
+        newName = str(fileName.text.replace(
+            fileType, fileNameToAdd)) + fileType
         fileName.text = str(newName)
     for path in root.iter('path'):
-        # newPath = str(path.text.replace('.jpg', fileNameToAdd)) + ".jpg"
-        newPath = address.replace('.jpg', fileNameToAdd) + ".jpg"
+        newPath = address.replace(fileType, fileNameToAdd) + fileType
         path.text = str(newPath)
+    for size in root.iter('size'):
+        size[0].text = str(augmentedImage.shape[1])
+        size[1].text = str(augmentedImage.shape[0])
+        size[2].text = str(augmentedImage.shape[2])
     i = 0
     for obj in root.findall('object'):
         if augmentedBBS.bounding_boxes[i].is_fully_within_image(augmentedImage.shape):
@@ -65,10 +73,10 @@ for address in imageAddresses:
         i += 1
 
     # augmentedImage = augmentedBBS.draw_on_image(
-    #    augmentedImage, size=5, color=[0, 0, 255])
+    #     augmentedImage, size=5, color=[0, 0, 255])
 
     # saving new xml
-    tree.write(newPath.replace('.jpg', '.xml'))
+    tree.write(newPath.replace(fileType, '.xml'))
     # saving new image
     imageio.imsave(newPath, augmentedImage)
 
