8ff408348ee3d95587a5cf8f0a5bbf2efcf0e18f
sky712345678 <37875281+sky712345678@users.noreply.github.com>
2020-09-29 22:16:02 +0800

    Bug fixes
    This version 2020/09/29:
    - Minor bug fixes

--- DataAugmentationGUI/App.py
+++ DataAugmentationGUI/App.py
@@ -30,7 +30,7 @@ class MainWindow(QMainWindow):
 
     def findAugmenterIndex(self, augmenterList, target, augmenterHistory):
         for i in range(len(augmenterList)):
-            if augmenterList[i][0].text == target and augmenterHistory[i] == 0:
+            if augmenterList[i][0].text == target and augmenterHistory[i] == 0 and augmenterList[i][1].text == 'Yes':
                 return i
         return - 1  # normally, it won't be executed
 
