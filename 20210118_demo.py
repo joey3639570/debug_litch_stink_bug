edbcb1f685d0f8e7ab0118d04a36e276e47eb62b
sky712345678 <37875281+sky712345678@users.noreply.github.com>
2021-01-20 22:06:12 +0800

    (in development)improved the UI
    New:
    - [UI]Added an instruction for picture selecting
    - [UI]Added some new running states to show
    - [UI]Enlarged some buttons
    - [Code]Improved the naming logic of the directory that stores detection outputs
    Fix:
    - [Code]Renamed some variable to make it more organized
    - [Code]Removed some redundant code

--- 20210118_demo.py
+++ 20210118_demo.py
@@ -24,108 +24,103 @@ class Ui_MainWindow(object):
         self.tabWidget = QtWidgets.QTabWidget(self.centralwidget) # parent is centralWidget
         self.tabWidget.setGeometry(QtCore.QRect(0, 0, 1280, 720))
         self.tabWidget.setObjectName("tabWidget")
-        self.tab = QtWidgets.QWidget() # declaration for tab, no inheritance needed
-        self.tab.setObjectName("tab")
+        self.tab1 = QtWidgets.QWidget() # declaration for tab, no inheritance needed
+        self.tab1.setObjectName("tab1")
         
         # Left grid for original picture
         '''
-        self.gridLayoutWidget = QtWidgets.QWidget(self.tab)
+        self.gridLayoutWidget = QtWidgets.QWidget(self.tab1)
         self.gridLayoutWidget.setGeometry(QtCore.QRect(10, 90, 201, 191))
         self.gridLayoutWidget.setObjectName("gridLayoutWidget")
         self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
         self.gridLayout.setObjectName("gridLayout")
         '''
-        self.tab1_left_picture = QtWidgets.QLabel(self)
+        self.tab1_left_picture = QtWidgets.QLabel(self.tab1)
         self.tab1_left_picture.setObjectName("tab1_left_picture")
         #self.gridLayout.addWidget(self.tab1_left_picture, 0, 0, 1, 1)
         
         # Right grid for detected picture
         '''
-        self.gridLayoutWidget_2 = QtWidgets.QWidget(self.tab)
+        self.gridLayoutWidget_2 = QtWidgets.QWidget(self.tab1)
         self.gridLayoutWidget_2.setGeometry(QtCore.QRect(240, 90, 201, 191))
         self.gridLayoutWidget_2.setObjectName("gridLayoutWidget_2")
         self.gridLayout_2 = QtWidgets.QGridLayout(self.gridLayoutWidget_2)
         self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
         self.gridLayout_2.setObjectName("gridLayout_2")
         '''
-        self.tab1_right_picture = QtWidgets.QLabel(self)
+        self.tab1_right_picture = QtWidgets.QLabel(self.tab1)
         self.tab1_right_picture.setObjectName("tab1_right_picture")
         #self.gridLayout_2.addWidget(self.tab1_right_picture, 0, 0, 1, 1)
+
+        # Label for selecting picture
+        self.tab1_picture_label = QtWidgets.QLabel(self.tab1)
+        self.tab1_picture_label.setObjectName("tab1_picture_label")
+        self.tab1_picture_label_font = QtGui.QFont()
+        self.tab1_picture_label_font.setPointSize(14)
+        self.tab1_picture_label.setFont(self.tab1_picture_label_font)
         
         # Button for selecting picture
-        self.tab1_picture_button = QtWidgets.QPushButton(self.tab) # parent is tab, which declares at line 26
-        #self.tab1_picture_button.setGeometry(QtCore.QRect(20, 40, 75, 23))
+        self.tab1_picture_button = QtWidgets.QPushButton(self.tab1)  # parent is tab, which declares at line 26
+        self.tab1_picture_button.setFixedWidth(120)
         self.tab1_picture_button.setMouseTracking(True)
         self.tab1_picture_button.setObjectName("tab1_picture_button")
+
         # Text for showing the path of the pic
-        self.tab1_picture_path_lineEdit = QtWidgets.QLineEdit(self.tab) # parent is tab
-        #self.tab1_picture_path_lineEdit.setGeometry(QtCore.QRect(110, 40, 331, 21))
+        self.tab1_picture_path_lineEdit = QtWidgets.QLineEdit(self.tab1) # parent is tab
         self.tab1_picture_path_lineEdit.setObjectName("tab1_picture_path_lineEdit")
         self.tab1_picture_path = ''
         
-        # Button for selecting model
-        '''self.tab1_model_button = QtWidgets.QPushButton(self.tab) # parent is tab
-        #self.tab1_model_button.setGeometry(QtCore.QRect(20, 10, 75, 23))
-        self.tab1_model_button.setMouseTracking(True)
-        self.tab1_model_button.setObjectName("tab1_model_button")
-        # Text for showing the path of the model
-        self.tab1_model_path = QtWidgets.QLineEdit(self.tab) # parent is tab
-        #self.tab1_model_path.setGeometry(QtCore.QRect(110, 10, 331, 21))
-        self.tab1_model_path.setObjectName("tab1_model_path")
-        self.tab1_model = '''''
-        
         # Running state
-        self.tab1_state = QtWidgets.QLabel(self.tab)
+        self.tab1_state = QtWidgets.QLabel(self.tab1)
         self.tab1_state.setObjectName("tab1_state")
         
         # Seperate line for buttons and pictures
-        self.tab1_line = QtWidgets.QFrame(self.tab) # parent is tab
-        #self.tab1_line.setGeometry(QtCore.QRect(0, 70, 461, 16))
+        self.tab1_line = QtWidgets.QFrame(self.tab1) # parent is tab
         self.tab1_line.setFrameShape(QtWidgets.QFrame.HLine)
         self.tab1_line.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.tab1_line.setObjectName("tab1_line")
 
         # Button for performing the test
-        self.tab1_test_button = QtWidgets.QPushButton(self.tab) # parent is tab
-        #self.tab1_test_button.setGeometry(QtCore.QRect(370, 290, 75, 23))
+        self.tab1_test_button = QtWidgets.QPushButton(self.tab1) # parent is tab
         self.tab1_test_button.setMouseTracking(True)
         self.tab1_test_button.setObjectName("tab1_test_button")
 
         # Nesting layout
-        self.tab1_verticalLayout = QtWidgets.QVBoxLayout(self.tab) # parent is tab. In order to line up widgets under self.tab???
-        # self.tab1_model_widget = QtWidgets.QWidget()
-        # self.tab1_model_widget_layout = QtWidgets.QHBoxLayout(self.tab1_model_widget) # parent is tab. In order to line up widgets under tab1_model_widget???
-        self.tab1_picture_path_widget = QtWidgets.QWidget()
-        self.tab1_picture_path_widget_layout = QtWidgets.QHBoxLayout(self.tab1_picture_path_widget) # parent is tab. In order to line up widgets under tab1_picture_path_widget???
+        self.tab1_verticalLayout = QtWidgets.QVBoxLayout(self.tab1)  # parent is tab. In order to line up widgets under self.tab1???
+        self.tab1_picture_description_widget = QtWidgets.QWidget()
+        self.tab1_picture_description_widget_layout = QtWidgets.QHBoxLayout(self.tab1_picture_description_widget)
+        self.tab1_picture_selection_widget = QtWidgets.QWidget()
+        self.tab1_picture_selection_widget_layout = QtWidgets.QHBoxLayout(self.tab1_picture_selection_widget)  # parent is tab. In order to line up widgets under tab1_picture_path_widget???
         self.tab1_picture_widget = QtWidgets.QWidget()
         self.tab1_picture_widget_layout = QtWidgets.QHBoxLayout(self.tab1_picture_widget) # parent is tab. In order to line up widgets under tab1_picture_widget???
         self.tab1_test_widget = QtWidgets.QWidget()
         self.tab1_test_widget_layout = QtWidgets.QVBoxLayout(self.tab1_test_widget) # parent is tab. In order to line up widgets under tab1_test_widget???
 
-        # add widget objects to layout object
-        # self.tab1_model_widget_layout.addWidget(self.tab1_model_button)
-        # self.tab1_model_widget_layout.addWidget(self.tab1_model_path)
+        # picture path widget
+        self.tab1_picture_description_widget_layout.addWidget(self.tab1_picture_label)
 
-        self.tab1_picture_path_widget_layout.addWidget(self.tab1_picture_button)
-        self.tab1_picture_path_widget_layout.addWidget(self.tab1_picture_path_lineEdit)
+        self.tab1_picture_selection_widget_layout.addWidget(self.tab1_picture_button)
+        self.tab1_picture_selection_widget_layout.addWidget(self.tab1_picture_path_lineEdit)
 
+        # pichture showing widget
         self.tab1_picture_widget_layout.addWidget(self.tab1_left_picture)
         self.tab1_picture_widget_layout.addWidget(self.tab1_right_picture)
 
+        # detection widget
         self.tab1_test_widget_layout.addWidget(self.tab1_test_button)
         self.tab1_test_widget_layout.addWidget(self.tab1_state)
 
-        # self.tab1_verticalLayout.addWidget(self.tab1_model_widget, alignment=QtCore.Qt.AlignTop)
-        self.tab1_verticalLayout.addWidget(self.tab1_picture_path_widget)
+        # whole layout
+        self.tab1_verticalLayout.addWidget(self.tab1_picture_description_widget)
+        self.tab1_verticalLayout.addWidget(self.tab1_picture_selection_widget)
         self.tab1_verticalLayout.addWidget(self.tab1_test_widget)
         self.tab1_verticalLayout.addWidget(self.tab1_line)
         self.tab1_verticalLayout.addWidget(self.tab1_picture_widget)
-
         self.tab1_verticalLayout.setAlignment(QtCore.Qt.AlignTop)
         
         # Add tab1 and things above into the tabWidget
-        self.tabWidget.addTab(self.tab, "")
+        self.tabWidget.addTab(self.tab1, "")
         '''
         # Tab2 - preprocessing
         self.tab_2 = QtWidgets.QWidget()
@@ -206,11 +201,11 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(_translate("MainWindow", "Tessaratoma Recognition"))
 
         # tab 1
-        self.tab1_picture_button.setText(_translate("MainWindow", "Select dir"))
-        # self.tab1_model_button.setText(_translate("MainWindow", "Select Model"))
+        self.tab1_picture_label.setText(_translate("MainWindow", "Click \"Browse\" to select the directory containing the pictures to detect"))
+        self.tab1_picture_button.setText(_translate("MainWindow", "Browse"))
         self.tab1_test_button.setText(_translate("MainWindow", "Detect"))
         self.tab1_state.setText(_translate("MainWindow", "Ready"))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "Model Test Demo"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab1), _translate("MainWindow", "Model Test Demo"))
         
         # tab 2
         #self.tab2_pic_button.setText(_translate("MainWindow", "Select PIC"))
@@ -252,14 +247,9 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             if self.tab1_picture_path_lineEdit.text() == '':
                 self.tab1_state.setText(self.tr('No directory selected'))
                 QtCore.QCoreApplication.processEvents(QtCore.QEventLoop.AllEvents)
-            else:
-                print(self.tab1_picture_path)
         return
 
     def on_tab1_test_button_Clicked(self):
-        '''if self.tab1_model == '':
-            print("No model!")
-            return'''
         if self.tab1_picture_path == '':
             print("No directory selected")
             self.tab1_state.setText(self.tr('No directory selected'))
@@ -286,12 +276,12 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             os.mkdir(detectionOutputPath)
             self.tab1_state.setText(self.tr('Created directory: ' + detectionOutputPath))
             QtCore.QCoreApplication.processEvents(QtCore.QEventLoop.AllEvents)
+        else:
+            self.tab1_state.setText(self.tr('Directory: ' + detectionOutputPath + ' already existed'))
+            QtCore.QCoreApplication.processEvents(QtCore.QEventLoop.AllEvents)
         
-        date = datetime.now().strftime("%Y%m%d%H%M")
-        resultDirectoryName = date
-        while (os.path.isdir(detectionOutputPath + resultDirectoryName) is True):
-            self.dateCount = self.dateCount + 1
-            resultDirectoryName = date + '_' + self.dateCount
+        date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        resultDirectoryName = date.replace('/', '').replace(':', '').replace(' ', '_')
         
         detectionOutputPath = detectionOutputPath + resultDirectoryName + '/'
         os.mkdir(detectionOutputPath)
@@ -397,6 +387,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             dominantObject = 'tessaratoma'
 
         statisticsDictionary = {
+            "time": date,
             "detection_output_path": detectionOutputPath,
             "detected_directory": self.tab1_picture_path,
             "dominant_object": dominantObject,
